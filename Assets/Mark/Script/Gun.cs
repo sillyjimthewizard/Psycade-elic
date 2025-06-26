e479ef2863a6268ded8efc8707cb9e6ab063cb5c
Markstag <BRE24003496@heartofyorkshire.ac.uk>
2025-06-26 10:36:09 +0100

    Invaders gun

---
+++ Assets/Mark/Script/Gun.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public Transform bulletSpawnPoint;
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 100;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+            bullet.GetComponent<Rigidbody>().velocity = new Vector3 (0,0,bulletSpeed);
+        }
+    }
+}
